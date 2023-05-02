d449f91b2df8432302a81c56708a9f35bc10cb24
Reed W Priest <pries153@umn.edu>
2023-05-02 13:01:13 -0500

    Data import and cleaning done. But check back later to ensure this is correct.

--- R/data_import.R
+++ R/data_import.R
@@ -3,8 +3,9 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 
 # Data Import and Cleaning
-# Start by importing the dataset tibble. It uses a + to separate data, so we use that as the delimiter.
-import_tbl_1 <- read_delim("../data/dataset.csv", delim = "+")
+# Start by importing the dataset as a tibble. It uses a plus sign to separate data, so we use that as the delimiter. I added an Employee_ID column indicating the row number to enable merging this tibble with the next tibble.
+import_tbl_1 <- read_delim("../data/dataset.csv", delim = "+") %>%
+  mutate(Employee_ID = row_number())
 # There are zero missing values in this tibble.
 sum(is.na(import_tbl_1))
 
@@ -13,3 +14,11 @@ import_tbl_2 <- read_delim("../data/satisfaction_reviews.csv", delim = ".", col_
   rename(c("The_Good" = X1, "The_Bad" = X2, "Employee_ID" = X3))
 # There are 106 missing values in this tibble.
 sum(is.na(import_tbl_2))
+# We are missing 32 employees' responses due to missing 32 Employee_ID numbers.
+1470 - nrow(import_tbl_2)
+
+# Join the two tibbles.
+full_tbl <- full_join(import_tbl_1, import_tbl_2, by = "Employee_ID") %>%
+  write_csv(file = "../data/full_dataset.csv")
+# There are 170 missing values in the merged tibble. This adds up. 170 - 106 = 64 total missing values across tibbles. 32 of these values are from missing ID numbers in both "The_Good" and 32 from "The_Bad" columns, because no data could be imported into these cells for the 32 employees that were missing from import_tbl_2. 
+sum(is.na(full_tbl))
