b0db8b7130ca221b1b6dbe7aea02e35ecc9ee6e7
Reed W Priest <pries153@umn.edu>
2023-05-03 12:26:06 -0500

    Reorganized the tables for H1:H3 to the Publications section. Added a scatterplot for H1. Added a Boxplot to H2.

--- R/statistical.R
+++ R/statistical.R
@@ -9,24 +9,39 @@ stat_tbl <- read.csv("../data/full_dataset.csv") %>%
 
 # Analyses
 # H1 Test: There is a relationship between monthly pay and performance rating. Correlation and significance test, with a scatterplot and fit line.
-(cor_test(stat_tbl, MonthlyIncome, PerformanceRating)) %>%
-  write_csv(file = "../out/H1.csv")
+cor_test(stat_tbl, MonthlyIncome, PerformanceRating)
 
 # H2 Test: Monthly pay differs by department. ANOVA and significance tests, with a boxplot split by department. Include a traditional ANOVA summary table (component name, SS, df, MS, F, p).
-(anova_test(stat_tbl, MonthlyIncome ~ Department, detailed = TRUE)) %>%
-  write_csv(file = "../out/H1a.csv")
+anova_test(stat_tbl, MonthlyIncome ~ Department, detailed = TRUE)
 # Post-hoc analyses since the ANOVA was signicicant.
-(tukey_hsd(stat_tbl, MonthlyIncome ~ Department)) %>%
-  write_csv(file = "../out/H1b.csv")
+tukey_hsd(stat_tbl, MonthlyIncome ~ Department)
 
 # H3 Test: Tenure can be predicted from relationship satisfaction, and this relationship is moderated by gender. Regression and significance tests, with scatterplot and fit lines. Note that you’ll need to plot predicted values (i.e., marginal effects), not raw data. Include a table of coefficients, t-tests, and p-values only (no SEs), with meaningful labels.
 int <- lm(YearsAtCompany ~ RelationshipSatisfaction * Gender, data = stat_tbl)
-(anova_test(int, detailed = TRUE)) %>%
-  write_csv(file = "../out/H3.csv")
+anova_test(int, detailed = TRUE)
 
 # Visualizations
+# H1 scatterplot. There was a perfectly bimodal distribution in performance rating scores, which obfuscated interpretations So I jittered the data. I considered adjusting the y-axis units to dispaly the full range of possible ranking values, but without context on that variable's full scale, I chose to leave the plotted scale as is.
+(ggplot(data = stat_tbl, aes(x = MonthlyIncome, y = PerformanceRating)) +
+    geom_jitter() +
+    geom_smooth(method = "lm", se = FALSE) +
+    labs(x = "Mothly Income", y = "Performance Rating")) %>%
+  ggsave(filename="../figs/H1.png", units="px", width=1920, height=1080)
+
+# H2 boxplot. 
+ggplot(data = stat_tbl, aes(x = Department, y = MonthlyIncome)) +
+  geom_boxplot() +
+  labs(x = "Departments", y = "Monthly Income")
 
 # Publication
 # Publication results for H1
 # Monthly pay and performance rating produced the following correlation: `r str_remove(round(cor_test(stat_tbl, MonthlyIncome, PerformanceRating)[[3]], 2), pattern = "^(-)?0")`. This was not a significant relationship at the alpha level of 0.05.
-  
+
+
+# Here are the three publication tables saved according to their hypothesis numbers.
+(cor_test(stat_tbl, MonthlyIncome, PerformanceRating)) %>%
+  write_csv(file = "../out/H1.csv")
+(tukey_hsd(stat_tbl, MonthlyIncome ~ Department)) %>%
+  write_csv(file = "../out/H1b.csv")
+(anova_test(int, detailed = TRUE)) %>%
+  write_csv(file = "../out/H3.csv")
